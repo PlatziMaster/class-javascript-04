90b7f45f0a51fc188a5c83e5d2a66d22a2c32bb3
Oscar Barajas Tavares <oscar@arepa.dev>
2020-02-12 00:13:16 -0500

    [Add] Testing

---
+++ src/__test__/index.test.js
@@ -0,0 +1,12 @@
+describe('Header', () => {
+  test('Test Title', () => {
+    expect('Hola Mundo').toMatch(/Mundo/);
+  });
+  test('Test Logo', () => {
+    expect('Oscar').toMatch(/Oscar/);
+  });
+  test('Test Nav', () => {
+    expect('Oscar').toMatch(/Oscar/);
+  });
+});
+
